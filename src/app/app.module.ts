8d97f3f70c6d55751a3ef811c125017bab54fafd
pevargasg <wearetamo@gmail.com>
2018-10-15 21:42:51 -0700

    added pipe to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AngularFirestoreModule } from "angularfire2/firestore";
 import { AngularFireStorageModule } from "angularfire2/storage";
 import { DropZoneDirective } from "./drop-zone.directive";
 import { FileUploadComponent } from './file-upload/file-upload.component';
+import { FileSizePipe } from './file-size.pipe';
 
 @NgModule({
-  declarations: [AppComponent, DropZoneDirective, FileUploadComponent],
+  declarations: [AppComponent, DropZoneDirective, FileUploadComponent, FileSizePipe],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),

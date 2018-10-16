e99564fbff838753d1b9fd4d37553cbcbb751843
pevargasg <wearetamo@gmail.com>
2018-10-15 20:59:53 -0700

    erased unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { UploadListComponent } from "./uploads/upload-list/upload-list.component";
-import { UploadFormComponent } from "./uploads/upload-form/upload-form.component";
 import { environment } from "../environments/environment";
 
 import { AngularFireModule } from "angularfire2";
 import { AngularFirestoreModule } from "angularfire2/firestore";
 import { AngularFireStorageModule } from "angularfire2/storage";
-import { UploadService } from "./uploads/shared/upload.service";
-import { DropZoneDirective } from './drop-zone.directive';
+import { DropZoneDirective } from "./drop-zone.directive";
 
 @NgModule({
-  declarations: [AppComponent, UploadListComponent, UploadFormComponent, DropZoneDirective],
+  declarations: [AppComponent, DropZoneDirective],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireStorageModule,
     AngularFirestoreModule
   ],
-  providers: [UploadService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

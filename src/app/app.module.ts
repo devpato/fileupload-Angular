24d2772df8bc21f4f4e4da8c535caf2163aae461
pevargasg <wearetamo@gmail.com>
2018-10-15 21:00:36 -0700

    file upload component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AngularFireModule } from "angularfire2";
 import { AngularFirestoreModule } from "angularfire2/firestore";
 import { AngularFireStorageModule } from "angularfire2/storage";
 import { DropZoneDirective } from "./drop-zone.directive";
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
-  declarations: [AppComponent, DropZoneDirective],
+  declarations: [AppComponent, DropZoneDirective, FileUploadComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),

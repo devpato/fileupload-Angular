d4843029bdcd7799314a522720a351cb8d072f1b
pevargasg <wearetamo@gmail.com>
2018-10-15 20:40:33 -0700

    added firebase modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { UploadListComponent } from './uploads/upload-list/upload-list.component';
-import { UploadFormComponent } from './uploads/upload-form/upload-form.component';
+import { AppComponent } from "./app.component";
+import { UploadListComponent } from "./uploads/upload-list/upload-list.component";
+import { UploadFormComponent } from "./uploads/upload-form/upload-form.component";
+import { environment } from "../environments/environment";
+
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { UploadService } from "./uploads/shared/upload.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UploadListComponent,
-    UploadFormComponent
-  ],
+  declarations: [AppComponent, UploadListComponent, UploadFormComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule.enablePersistence()
   ],
-  providers: [],
+  providers: [UploadService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+//npm install firebase angularfire2
